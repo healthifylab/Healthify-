614afd5bc5c2131d3cd7052ffd6dd7b406eaac01
healthifylab <gag92gag92@gmail.com>
2025-06-30 14:41:52 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '20px', background: '#f0f0f0' }}>
+      <p>📞 <strong>+91 9503832889</strong></p>
+      <p>📧 <strong>report@healthifylab.com</strong></p>
+      <p>&copy; 2025 Healthify Lab. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
