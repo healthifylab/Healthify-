7c03a0b9de13ca2ad0dc9c259c02025441682e6d
healthifylab <gag92gag92@gmail.com>
2025-06-30 10:22:28 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
 import Booking from "./Booking.jsx";
 import Admin from "./Admin.jsx";
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Navigate to="/booking" />} />
+      <Route path="/" element={<Home />} />
       <Route path="/booking" element={<Booking />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
